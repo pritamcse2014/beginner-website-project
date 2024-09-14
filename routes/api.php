5be5fe1795cbfea120dd9dfb1e1da4a96cde61f0
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-09-14 20:56:06 +0600

    Controller-Methods-Routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ResumeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//    AJAX Call Routes
+Route::get('/heroData', [HomeController::class, 'heroData']);
+Route::get('/aboutData', [HomeController::class, 'aboutData']);
+Route::get('/socialData', [HomeController::class, 'socialData']);
+Route::get('/projectsData', [ProjectController::class, 'projectData']);
+Route::get('/resumeLink', [ResumeController::class, 'resumeLink']);
+Route::get('/experiencesData', [ResumeController::class, 'experiencesData']);
+Route::get('/educationData', [ResumeController::class, 'educationData']);
+Route::get('/skillsData', [ResumeController::class, 'skillsData']);
+Route::get('/languageData', [ResumeController::class, 'languageData']);
+Route::post('/contactRequest', [ContactController::class, 'contactRequest']);
