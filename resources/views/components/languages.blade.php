ff94d68a2a6bfb5e9f3b63e703ae9546a9534d2b
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-09-16 14:58:35 +0600

    Resume-Languages-Ajax-Call

--- resources/views/components/languages.blade.php
+++ resources/views/components/languages.blade.php
@@ -6,15 +6,10 @@
                 <h2 class="text-secondary fw-bolder mb-4">Languages</h2>
                 <div class="card shadow border-0 rounded-4 mb-5">
                     <div class="card-body p-5">
-                        <div class="row row-cols-1 row-cols-md-3 mb-4">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">HTML</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">CSS</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">JavaScript</div></div>
-                        </div>
-                        <div class="row row-cols-1 row-cols-md-3">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Python</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Ruby</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Node.js</div></div>
+                        <div id="language-list" class="row row-cols-1 row-cols-md-3 mb-4">
+                            
+
+
                         </div>
                     </div>
                 </div>
@@ -22,3 +17,25 @@
         </div>
     </div>
 </div>
+
+<script>
+    getLanguageList()
+    async function getLanguageList() {
+        let URL = '/languageData';
+        // let response = await axios.get(URL);
+        try {
+            let response = await axios.get(URL);
+            response.data.forEach( (item) => {
+                document.getElementById('language-list').innerHTML += (
+                    `
+                        <div class="col p-2 mb-4 mb-md-0">
+                            <div class="d-flex align-items-center justify-content-center bg-light rounded-4 p-3 h-100">${item['name']}</div>
+                        </div>
+                    `
+                )
+            } )
+        } catch (error) {
+            alert(error);
+        }
+    }
+</script>
