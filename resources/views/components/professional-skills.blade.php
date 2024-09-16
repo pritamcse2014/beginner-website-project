60aed703846b9f93b62c1a375c26938e5bf0b968
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-09-16 15:28:32 +0600

    Resume-Skills-Ajax-Call

--- resources/views/components/professional-skills.blade.php
+++ resources/views/components/professional-skills.blade.php
@@ -6,15 +6,10 @@
                 <h2 class="text-secondary fw-bolder mb-4">Professional Skills</h2>
                 <div class="card shadow border-0 rounded-4 mb-5">
                     <div class="card-body p-5">
-                        <div class="row row-cols-1 row-cols-md-3 mb-4">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">SEO/SEM Marketing</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Statistical Analysis</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web Development</div></div>
-                        </div>
-                        <div class="row row-cols-1 row-cols-md-3">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Network Security</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Adobe Software Suite</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
+                        <div id="skills-list" class="row row-cols-1 row-cols-md-3 mb-4">
+                            
+
+
                         </div>
                     </div>
                 </div>
@@ -22,3 +17,25 @@
         </div>
     </div>
 </div>
+
+<script>
+    getSkillsList()
+    async function getSkillsList() {
+        let URL = '/skillsData';
+        // let response = await axios.get(URL);
+        try {
+            let response = await axios.get(URL);
+            response.data.forEach( (item) => {
+                document.getElementById('skills-list').innerHTML += (
+                    `
+                        <div class="col p-2 mb-4 mb-md-0">
+                            <div class="d-flex align-items-center justify-content-center bg-light rounded-4 p-3 h-100">${item['name']}</div>
+                        </div>
+                    `
+                )
+            } )
+        } catch (error) {
+            alert(error);
+        }
+    }
+</script>
